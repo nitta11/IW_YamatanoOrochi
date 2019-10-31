6d81115fc447b5d4cd8f9cb19663a793539c2a1b
nitta11 <2li2think@gmail.com>
2019-10-31 15:59:44 +0900

    要素の追加
    タイトルシーンにStartとFnishボタンのオブジェクト追加
    Oculusリモコンの表示機能を追加
    おそらくリモコンのポインタ機能実装

---
+++ IW_YamatanoOrochi/Assets/ButtonToStory1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonToStory1 : MonoBehaviour
+{
+
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene("Story1");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ IW_YamatanoOrochi/Assets/LaserPointer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _RightHandAnchor;
+
+    [SerializeField]
+    private Transform _LeftHandAnchor;
+
+    [SerializeField]
+    private Transform _CenterEyeAnchor;
+
+    [SerializeField]
+    private float _MaxDistance = 100.0f;
+
+    [SerializeField]
+    private LineRenderer _LaserPointerRenderer;
+
+    private Transform Pointer
+    {
+        get
+        {
+            // 現在アクティブなコントローラーを取得
+            var controller = OVRInput.GetActiveController();
+            if (controller == OVRInput.Controller.RTrackedRemote)
+            {
+                return _RightHandAnchor;
+            }
+            else if (controller == OVRInput.Controller.LTrackedRemote)
+            {
+                return _LeftHandAnchor;
+            }
+            // どちらも取れなければ目の間からビームが出る
+            return _CenterEyeAnchor;
+        }
+    }
+
+    void Update()
+    {
+        var pointer = Pointer;
+        if (pointer == null || _LaserPointerRenderer == null)
+        {
+            return;
+        }
+        // コントローラー位置からRayを飛ばす
+        Ray pointerRay = new Ray(pointer.position, pointer.forward);
+
+        // レーザーの起点
+        _LaserPointerRenderer.SetPosition(0, pointerRay.origin);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(pointerRay, out hitInfo, _MaxDistance))
+        {
+            // Rayがヒットしたらそこまで
+            _LaserPointerRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            // Rayがヒットしなかったら向いている方向にMaxDistance伸ばす
+            _LaserPointerRenderer.SetPosition(1, pointerRay.origin + pointerRay.direction * _MaxDistance);
+        }
+    }
+
+}
